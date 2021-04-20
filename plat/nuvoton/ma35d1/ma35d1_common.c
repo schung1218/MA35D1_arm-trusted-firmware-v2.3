cd0420a55310bcd891d4221514ddbcaa03f46dcc
Y.C. Huang <ychuang3@nuvoton.com>
2021-04-20 11:58:25 +0800

    Fix compile error of ma35d1_comm.c.
    Change-Id: I00491e822e1928d4eb919cbe6c08f3a18a7159d9

--- plat/nuvoton/ma35d1/ma35d1_common.c
+++ plat/nuvoton/ma35d1/ma35d1_common.c
@@ -261,8 +261,8 @@ void plat_ma35d1_init(void)
 
 		/* Init KeyStore */
 		outp32((void *)(KS_BASE+0x00), 0x101);		/* KS INIT(KS_CTL[8]) + START(KS_CTL[0]) */
-		while ((inp32(void *)(KS_BASE+0x08) & 0x100) == 0);   /* wait for INITDONE(KS_STS[8]) set */
-		while (inp32(void *)(KS_BASE+0x08) & 0x4);      /* wait for BUSY(KS_STS[2]) cleared */
+		while ((inp32((void *)(KS_BASE+0x08)) & 0x100) == 0);   /* wait for INITDONE(KS_STS[8]) set */
+		while (inp32((void *)(KS_BASE+0x08)) & 0x4);      /* wait for BUSY(KS_STS[2]) cleared */
 	}
 
 	node = fdt_node_offset_by_compatible(fdt, -1, "nuvoton,ma35d1-sspcc");
