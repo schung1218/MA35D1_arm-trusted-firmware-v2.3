41c6d991cd91793cc5026fa7bb98631f926ff71b
schung <schung@nuvoton.com>
2021-09-03 13:48:45 +0800

    Add ma35d1xx0.dts
    Change-Id: I4ec53a2e5c942fa1c3436228248e184c41acaeb2

--- plat/nuvoton/ma35d1/platform.mk
+++ plat/nuvoton/ma35d1/platform.mk
@@ -66,7 +66,7 @@ PLAT_INCLUDES		:=	-Iplat/nuvoton/ma35d1/include		\
 				-Iinclude/plat/arm/common/aarch64
 
 # Device tree
-DTB_FILE_NAME		?=	ma35d1xx8.dtb ma35d1xx7.dtb
+DTB_FILE_NAME		?=	ma35d1xx8.dtb ma35d1xx7.dtb ma35d1xx0.dtb
 FDT_SOURCES		:=	$(addprefix fdts/, $(patsubst %.dtb,%.dts,$(DTB_FILE_NAME)))
 DTC_FLAGS		+=	-Wno-unit_address_vs_reg
 
